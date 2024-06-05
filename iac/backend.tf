de6a35c84695d746701cb58eb2dd48ac2f286d03
Long Pham <long20p@gmail.com>
2024-06-05 23:12:31 +0200

    Update backend storage resource group

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     # modify the values for the storage account
-    resource_group_name  = "rg-sagalogic-dev"
+    resource_group_name  = "rg-sagalogic-tf"
     storage_account_name = "sttfsagaorch"
     container_name       = "testingterraform"
     key                  = "terraform.tfstate"
